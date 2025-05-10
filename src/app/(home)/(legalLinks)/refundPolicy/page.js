f533dbe6b3d5bfce2f8ccdeb7bd512dd05878da6
SUDARSHAN NATH MAZUMDAR <118620928+sudarshannathmazumdar123@users.noreply.github.com>
2025-05-10 18:40:20 +0530

    Update page.js

--- src/app/(home)/(legalLinks)/refundPolicy/page.js
+++ src/app/(home)/(legalLinks)/refundPolicy/page.js
@@ -18,7 +18,7 @@ export default function RefundPolicy() {
                     <div className="hover:bg-blue-50 p-6 rounded-xl transition-colors duration-300">
                         <h2 className="text-2xl font-semibold text-primary mb-4">2. Exceptions</h2>
                         <p className="text-gray-700 leading-relaxed">
-                            In rare cases where there may be a billing error or technical issue, we may review the situation. However, refunds are not guaranteed and are assessed on a case-by-case basis at the sole discretion of Indian Cyber Excellence Institute.
+                            In rare cases where there may be a billing error or technical issue, we may review the situation. However, refunds are not guaranteed and are assessed on a case-by-case basis at the sole discretion of SUDAMASOLUTIONS TECHNOLOGIES & HEALTH CARE PVT LTD.
                         </p>
                     </div>
 
